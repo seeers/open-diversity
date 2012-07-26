9778854db2dbf1f185f3df293ca16e50df3566a8
nils.huntemann <nils.huntemann@gmx.de>
2012-07-26 22:57:33 +0000

    Buzzer alarm on low voltage and to indicate channel jumps, based on the software for the alphaHW.

--- open-diversity HW1.0/op_diversity/op_diversity.ino
+++ open-diversity HW1.0/op_diversity/op_diversity.ino
@@ -2,9 +2,9 @@
 Open Diversity - Video based diversity software
 Idea by Rangarid
 Hardware design by Daniel
-Implementation and refinement by Nils, Nachbrenner, Rangarid
+Implementation and refinement by Nils, Nachbrenner
 
-Copyright 2011-2012 by Nils, Nachbrenner, Rangarid
+Copyright 2011-2012 by Nils, Nachbrenner
 
 This file is part of Open Diversity
 
@@ -23,36 +23,35 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */
 
 #include <FlexiTimer2.h> // http://www.pjrc.com/teensy/td_libs_MsTimer2.html
+
 #include "config.h"
-#include "defines.h"
 #include "uart_custom.h"
 
 
 volatile long unsigned int numSyncs[2] = {0, 0};
 volatile long unsigned int lastPulses[2][HISTORY_SIZE]; 
-volatile int activeSource = 0;
 volatile int lastPulseSlot = 0; // to use lastPulse-Array as ring buffer
 volatile int numTimerEvents = 0;
 unsigned int inputVoltage;
 unsigned int sensitivity;
 unsigned int SWITCH_FPS_RESIST = 4 ; // How many FPS the video sources must differ to recitify a switch
-boolean buzzerOn = false;
+volatile char buzzerStatus = 0; // 0 ^= no alarm, 1 an -1 ^= switched to source 1, 2 an -2 ^= switched to source 2 , 10,-10,5,-5 low Voltage, 100 be quiet!
+
+volatile int activeSource = 0;
 
 void switchToVideoSource(int source, int fr0,  int fr1) {
   if (source == 0) {
     digitalWrite(VSWITCH_SELECT, HIGH);
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, LOW);
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, LOW);
     activeSource=0;
   } else {
     digitalWrite(VSWITCH_SELECT, LOW);
-    digitalWrite(LED1, LOW);
-    digitalWrite(LED2, HIGH);
+    digitalWrite(LED_1, LOW);
+    digitalWrite(LED_2, HIGH);
     activeSource=1;    
   }
-  #ifdef DEBUG
-    printf("Switching to source %d (%d vs %d)\n", source, fr0, fr1);
-  #endif
+  printf("Switching to source %d (%d vs %d)\n", source, fr0, fr1);
 }
 
 void VSVideo1() {
@@ -63,26 +62,30 @@ void VSVideo2() {
   numSyncs[1]++;
 }
 
+
 // Calculate average frame rate based on last recorded pulses
 unsigned int calcAvgFrameRate(int videoIn) {
   unsigned int sum = 0;
   
   for(int i=0;i<HISTORY_SIZE;i++) {
-      sum+=lastPulses[videoIn][i] * (1000/FR_UPDATE_RATE);;
+      sum+=lastPulses[videoIn][i] * (1000/FR_UPDATE_INT);;
   }
   return (sum/HISTORY_SIZE);
 }
 
+
 void runTimer() {
+  
   // Update framerate
   lastPulseSlot = (lastPulseSlot+1) % HISTORY_SIZE;
   lastPulses[0][lastPulseSlot] = numSyncs[0];
   lastPulses[1][lastPulseSlot] = numSyncs[1];
   numSyncs[0] = numSyncs[1] = 0;
   
-  // check if we need to check for video switch. if yes: do it! :)
+  
+  // see if we need to check for video switch. if yes: do it! :)
   numTimerEvents++;
-  if (numTimerEvents >= SWITCH_EVERY) {
+  if (numTimerEvents % SWITCH_EVERY == 0) {
     numTimerEvents = 0;
     
     int fr0 = calcAvgFrameRate(0);
@@ -90,32 +93,91 @@ void runTimer() {
   
     if ((abs(fr1 - 50) + SWITCH_FPS_RESIST < abs(fr0 - 50)) && activeSource == 0) {
       switchToVideoSource(1, fr0, fr1);
+      if ( buzzerStatus == 0) {
+#ifdef BUZZER_CHANNEL_IND
+        buzzerStatus = 2 ; 
+#endif
+      }
     } else if ( (abs(fr0 - 50) + SWITCH_FPS_RESIST < abs(fr1 - 50)) && activeSource == 1) {
       switchToVideoSource(0, fr0, fr1);
+       if ( buzzerStatus == 0) {
+#ifdef BUZZER_CHANNEL_IND
+         buzzerStatus = 1 ; 
+#endif
+      }
     }
     
-    #ifdef DEBUG
-      #ifdef RSSI_ON
-        //rssi0 und rssi1 müssen vorher deklariert werden, damit du nach #ifdef RSSI_ON noch drauf zugreifen kannst...
-        int rssi0 = analogRead(RSSI1);
-        int rssi1 = analogRead(RSSI2);
-        printf("Last Frames: %d (RSSI: %d) / %d (RSSI: %d)\n",  fr0, rssi0, fr1, rssi1);
-      #else
-        printf("Last Frames: %d / %d\n",  fr0, fr1);
-      #endif
-    #endif
+#ifdef RSSI_ON
+    int rssi0 = analogRead(RSSI1);
+    int rssi1 = analogRead(RSSI2);
+#ifdef SERIAL_OUTPUT_ON
+    printf("Last Frames: %d (%d) / %d (%d)\n",  fr0, rssi0, fr1, rssi1);
+#endif
+#else
+#ifdef SERIAL_OUTPUT_ON
+    printf("Last Frames: %d / %d\n",  fr0, fr1);
+#endif
+#endif
+
   }
   
+    if (numTimerEvents % BEEP_EVERY == 0) { 
+    if ( buzzerStatus != 0 ) {
+      switch (buzzerStatus) {
+        case 1: 
+         digitalWrite(BUZZER, LOW);
+         buzzerStatus = -1;
+         break;
+        
+        case -1: 
+         digitalWrite(BUZZER, HIGH);
+         buzzerStatus = 100;
+         break;
+        
+        case 2: 
+         digitalWrite(BUZZER, LOW);
+         buzzerStatus = -2;
+         break;
+        
+        case -2: 
+         digitalWrite(BUZZER, HIGH);
+         buzzerStatus = 1;
+         break;
+         
+        case 10: 
+         digitalWrite(BUZZER, LOW);
+         buzzerStatus = 5;
+         break;
+        
+        case 5: 
+         digitalWrite(BUZZER, LOW);
+         buzzerStatus = -5;
+         break;
+         
+        case -5: 
+         digitalWrite(BUZZER, HIGH);
+         buzzerStatus = -10;
+         break;
+         
+        case -10: 
+         digitalWrite(BUZZER, HIGH);
+         buzzerStatus = 10;
+         break;
+         
+        default: 
+         digitalWrite(BUZZER, LOW);
+         buzzerStatus = 0;
+        }
+    } 
+  } 
 }
 
 
 // =====================================================================
 
 void setup() {
-  #ifdef DEBUG
-    initUART(); // for printf to serial
-  #endif
   
+  initUART(); // for printf to serial
   // Initialize sync-vars
   for(int i=0;i<HISTORY_SIZE;i++) {
     lastPulses[0][i]=0;
@@ -125,41 +187,47 @@ void setup() {
   pinMode(VSWITCH_SELECT, OUTPUT);
   pinMode(VSWITCH_DISABLE, OUTPUT);
   digitalWrite(VSWITCH_DISABLE, LOW);
-  
   pinMode(BUZZER, OUTPUT);
-  #ifdef BEEPONSTART
-    digitalWrite(BUZZER, HIGH);
-    delay(500);
-  #endif
-  digitalWrite(BUZZER, LOW);  
-  
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  digitalWrite(LED1, LOW);
-  digitalWrite(LED2, LOW); 
-  
+  digitalWrite(BUZZER, LOW);
+  pinMode(LED_1, OUTPUT);
+  digitalWrite(LED_1, LOW);
+  pinMode(LED_2, OUTPUT);
+  digitalWrite(LED_2, LOW); 
   pinMode(SUP_VOLT, INPUT);
-  pinMode(SENS_INPUT, INPUT); 
-  
+  pinMode(sensitivity, INPUT); 
   attachInterrupt(1, VSVideo1, RISING);
   attachInterrupt(0, VSVideo2, RISING);
   
   
   // Timer to update frame rate records and switch video
-  FlexiTimer2::set(FR_UPDATE_RATE, runTimer);
+  FlexiTimer2::set(FR_UPDATE_INT, runTimer);
   FlexiTimer2::start();
   
   switchToVideoSource(0,0,0);
+  digitalWrite(LED_1, HIGH);
+  digitalWrite(BUZZER, LOW);
 }
 
 void loop() {
- //inputVoltage = analogRead(SUP_VOLT);
- SWITCH_FPS_RESIST = map(analogRead(SENS_INPUT),0,1023,0,10);
-  
-  /*
-  #ifdef DEBUG
-    printf("input Voltage: %d \n", inputVolt);
-    printf("SWITCH_FPS_RESIST: %d \n", SWITCH_FPS_RESIST);
-  #endif
-  */
+ delay(1000);
+  printf("%d",buzzerStatus);
+ #ifdef LOWVOLTAG_WARN
+  inputVoltage=analogRead(SUP_VOLT);
+ if ( inputVoltage < ALARM_VOLT && abs(buzzerStatus) < 4 )
+  { buzzerStatus = 10 ;
+   }
+ if ( inputVoltage > ALARM_VOLT && abs(buzzerStatus) > 4)
+  { buzzerStatus = 100 ;
+  }
+ #endif
+ 
+  SWITCH_FPS_RESIST=map(analogRead(SENS_INPUT),0,1023,0,50);
+#ifdef SERIAL_OUTPUT_ON
+  printf("input Voltage: %d \n", inputVoltage);
+  printf("SWITCH_FPS_RESIST: %d \n", SWITCH_FPS_RESIST);
+#endif
+ // switchToVideoSource(1, 0,0);
+  delay(1000);
+ // digitalWrite(BUZZER, LOW);
+ //  switchToVideoSource(0, 0,0);
 }
