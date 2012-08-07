d4b75c3f77cd8dbbe81ee15980b755fd4cc46736
nils.huntemann@gmx.de <nils.huntemann@gmx.de@b3bded50-3aec-70f9-0e44-aaa661398c66>
2012-08-07 20:19:31 +0000

    Buzzer function is switchable by #1 and #2 of the dip switch.

--- open-diversity HW1.0/op_diversity/config.h
+++ open-diversity HW1.0/op_diversity/config.h
@@ -21,6 +21,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. */
+
 // PINS
 #define VSWITCH_DISABLE 11
 #define VSWITCH_SELECT 10
@@ -30,23 +31,20 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
 #define BUZZER 8
 
 #define SUP_VOLT A0
+#define DIP_SWITCH A1
+#define RSSI1 A2 
+#define RSSI2 A3 
 #define SENS_INPUT A4
-#define LOWVOLTAG_WARN
-#define ALARM_VOLT 540 // to calculate, use U / 20.15 V *1023, where U is the alarm voltage in Volts
 
+// ===================
+
+#define ALARM_VOLT 540 // to calculate, use U / 20.15 V *1023, where U is the alarm voltage in Volts
 #define HISTORY_SIZE 10 // How many old framerates to keep to calculate avg
 #define FR_UPDATE_INT 100 // update framerate records every XX milliseconds
 #define SWITCH_INT 200 // look if I need to switch video input every XXX milliseconds. Needs to be > FR_UPDATE_RATE
 #define BEEP_INT 400
 
-
-// === Optional RSSI
-
-//#define RSSI_ON // comment out to disable RSSI output
 //#define SERIAL_OUTPUT_ON
-#define BUZZER_CHANNEL_IND // acoustic channel chance indicator
-#define RSSI1 A3 
-#define RSSI2 A2 // A0 ist the Pin that is next to yellow led
 
 // ===================
 
